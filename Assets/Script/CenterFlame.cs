d7578600355f562f0409493fea991f1dbbaca964
gamegame2 <gamegame>
2021-02-23 16:32:53 +0900

    노트

---
+++ Assets/Script/CenterFlame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CenterFlame : MonoBehaviour
+{
+    AudioSource myAudio;
+    bool musicStart = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAudio = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!musicStart && collision.CompareTag("Note"))
+        {
+            myAudio.Play();
+            musicStart = true;
+        }
+    }
+}
---
+++ Assets/Script/Manager/NoteManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteManager : MonoBehaviour
+{
+    public int bpm = 0;
+    double currenTime = 0d;
+
+    [SerializeField] Transform tfNoteAppcar = null;
+    [SerializeField] GameObject goNote = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currenTime += Time.deltaTime;
+
+        if(currenTime >= 60d / bpm)
+        {
+            GameObject t_note = Instantiate(goNote, tfNoteAppcar.position, Quaternion.identity);
+            t_note.transform.SetParent(this.transform);
+            currenTime -= 60d / bpm;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Note"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
